47a7c3a8759f25215a36ae28b68c26e1dbdc4dc0
Sungmin <sungmin.know@gmail.com>
2024-01-27 11:38:16 +0000

    퐁 게임을 위한 함수 추가

--- 2.component/PongGame.py
+++ 2.component/PongGame.py
@@ -1,11 +1,33 @@
-class ball():
+import math
+import time
+
+def window(lowerbound, x, upperbound):
+    return max(lowerbound, min(x, upperbound))
+
+def normalize_vec2(tuple_vector):
+    # 튜플의 요소 추출
+    a, b = tuple_vector
+
+    # 벡터의 크기 계산
+    magnitude = math.sqrt(a**2 + b**2)
+
+    # 정규화된 벡터 계산
+    normalized_vector = (a / magnitude, b / magnitude)
+
+    return normalized_vector
+
+class obje():
     def __init__(self) -> None:
         self.position = tuple(float, float)
         self.velocity = tuple(float, float)
         self.reftime = float # 위치와 속도가 업데이트된 시간
         pass
 
-    def updateStatue(self, position, velocity, time): pass
+    def updateStatue(self, position, velocity, time):
+        self.position = position
+        self.velocity = velocity
+        self.reftime = time
+
     def updateVelocity(self, velocity, time):
         self.position = self.getPosition(time)
         self.velocity = velocity
@@ -18,22 +40,18 @@ class ball():
         
     def getVelocity(self, time) -> tuple(float, float): pass
 
+class ball(obje):
+    pass
 
-class player():
-    def __init__(self) -> None:
-        self.position = tuple(float, float)
-        self.velocity = tuple(float, float)
-        self.reftime = float # 위치와 속도가 업데이트된 시간
-        pass
-
-    def updateStatue(position, velocity, time): pass
-
-    def getPosition(time) -> tuple(float, float): pass
-    def getVelocity(time) -> tuple(float, float): pass
+class paddle(obje):
+    def getPosition(self, time) -> tuple(float, float):
+        #return (time - self.reftime) * self.velocity + self.position  (이건 넘파이 수식)
+        timediff = (time - self.reftime)
+        return tuple(self.position[0], window(0, timediff * self.velocity[1] + self.position[1], 1))
 
 
 
-import time
+
 class timer():
     def __init__(self) -> None:
         self.start_time = None
@@ -72,12 +90,24 @@ class PongGame():
 
         score = (0,0)
 
+        paddleWidth = 0.02
+        paddleHeight = 0.16
+        
+        paddleSpeed = 0.2
+        ballSpeed = 0.3
+
+        teamRedWall = -1
+        teamBlueWall = 1
+
+
+
+
         paddleline = []
         # 패들의 x위치 지정
         paddleline[1] = (0.06 - 1) #왼쪽의 플레이어
         paddleline[2] = - (0.06 - 1)#오른쪽의 플레이어
-        paddleline[3] = (0.06 - 1) #왼쪽의 추가 플레이어
-        paddleline[4] = - (0.06 - 1)#오른쪽의 추가 플레이어
+        paddleline[3] = (0.4 - 1) #왼쪽의 추가 플레이어
+        paddleline[4] = - (0.4 - 1)#오른쪽의 추가 플레이어
 
         pass
 
@@ -87,4 +117,18 @@ class PongGame():
         paddle2 = (-(0.06 - 1), 1/2) #오른쪽의 플레이어
 
 
-    def game()
+    def mini_game():
+        timer1 = timer()
+
+        gamestat = 
+
+    def mini_gameinput(playernum, direction):
+        pass
+        
+
+
+
+    def gameinput(playerIdentifier, direction):
+        #player는 플레이어 식별자.
+        #directoin은 up down stop 중 하나
+        pass
