4754522196c75f6a5c299603ca0bd8fa06d7e3f5
Sungmin <sungmin.know@gmail.com>
2024-02-28 16:01:23 +0000

    웹소켓 테스터를 위한 프로그램 설치

--- backend/matchserver/test_websocket.py
+++ backend/matchserver/test_websocket.py
@@ -33,7 +33,7 @@ def on_open(ws):
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://your-websocket-server-url",
+    ws = websocket.WebSocketApp("wss://nginx/ws/",
                               on_message=on_message,
                               on_error=on_error,
                               on_close=on_close)
