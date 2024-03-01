63f6a7a0f90d95e68b44127fad299548b1172d2c
Sungmin <sungmin.know@gmail.com>
2024-03-01 20:21:29 +0000

    요청에 의한 수정

--- backend/matchserver/consumers.py
+++ backend/matchserver/consumers.py
@@ -149,7 +149,7 @@ class MyConsumer(AsyncWebsocketConsumer):
             'method': method,
             'status': status,
             'identify': identify,
-            'response_Id': self.user_id,
+            'responseId': self.user_id,
             'data': data or {}  # data가 None이면 빈 딕셔너리를 반환
         }
         print("send : " , response)
