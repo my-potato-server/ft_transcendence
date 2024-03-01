16937814ac05a1c4bd6aaea49184f567267bf2c1
progresshans <progresshans@gmail.com>
2024-03-02 03:22:12 +0900

    templogin 서버랑 연동하도록 수정

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -92,17 +92,61 @@ export default class Login extends Component {
 	templogin() {
 		console.log("templogin");
 		sessionStorage.setItem('auth', true);
-		sessionStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozNCwibG9naW4iOiJzdHJpbmciLCJuaWNrbmFtZSI6InN0cmluZyIsImV4cCI6MTcxMTkwMjQyOH0.WjVhrYweWouO6y5jvZuBnHwbziNliq2p3OH7sJIcvks');
-		sessionStorage.setItem('userinfo', JSON.stringify({user:{
-			id : 34,
-			login : 'string',
-			nickname : 'string',
-			image : '/media/default.png',
-		}}));
-		this.$parent.auth = true;
-		this.$parent.userinfo = JSON.stringify(sessionStorage.getItem('userinfo'));
-		this.$parent.token = sessionStorage.getItem('token');
-		this.setState({locate: '/src/pages/Main'});
+		fetch('/api/account/make-test-user', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ login: 'temp' })
+		})
+		.then(response => {
+			if (response.ok) {
+				console.log("templogin 성공");
+				return response.json();
+			} else {
+				console.log("templogin 실패");
+				alert("templogin 실패");
+			}
+		})
+		.then(data => {
+			const token = data.token;
+			console.log("token", token);
+			this.$parent.auth = true;
+            this.$parent.token = token;
+            this.lauth = true;
+            this.ltoken = token;
+            return fetch('/account/me', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'authorization': 'Bearer ' + token,
+            }})
+		})
+		.then(async response => {
+			console.log("response", response);
+            const infos = await response.json();
+            console.log(infos);
+            this.luserinfo = JSON.stringify(infos);
+            this.$parent.userinfo = JSON.stringify(infos);
+            sessionStorage.setItem('userinfo', JSON.stringify(infos));
+            // this.setState({ locate: '/src/pages/Main'});
+            sessionStorage.setItem('auth', this.lauth);
+            sessionStorage.setItem('token', this.ltoken);
+            sessionStorage.setItem('userinfo', this.luserinfo);
+            console.log("templogins", this.logins);
+            this.setState({ locate: '/src/pages/Main'});
+		})
+		// sessionStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozNCwibG9naW4iOiJzdHJpbmciLCJuaWNrbmFtZSI6InN0cmluZyIsImV4cCI6MTcxMTkwMjQyOH0.WjVhrYweWouO6y5jvZuBnHwbziNliq2p3OH7sJIcvks');
+		// sessionStorage.setItem('userinfo', JSON.stringify({user:{
+		// 	id : 34,
+		// 	login : 'string',
+		// 	nickname : 'string',
+		// 	image : '/media/default.png',
+		// }}));
+		// this.$parent.auth = true;
+		// this.$parent.userinfo = JSON.stringify(sessionStorage.getItem('userinfo'));
+		// this.$parent.token = sessionStorage.getItem('token');
+		// this.setState({locate: '/src/pages/Main'});
 	}
 
 	// getAuth() {
